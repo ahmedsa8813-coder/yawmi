786154e190d0ec4d8a1c3722b99974ed4baad636
ahmedsa8813-coder <ahmed.sa8813@gmail.com>
2026-08-14 14:41:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mb-v9';const FILES=['./',  './index.html','./manifest.json','./icon.png'];
+const CACHE='mb-v10';const FILES=['./',  './index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res;}).catch(()=>caches.match(e.request)));});
